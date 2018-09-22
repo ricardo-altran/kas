06c9abc888e8dcabb2809bb1edba35bbe0f82378
ricardo-altran <ricardo-altran@outlook.com>
2018-09-22 20:18:47 +0200

    Little modification to controller integration test

--- src/test/java/com/altran/techtest/kas/controller/ResourceReaderControllerTest.java
+++ src/test/java/com/altran/techtest/kas/controller/ResourceReaderControllerTest.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.Is.is;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -48,7 +49,8 @@ public class ResourceReaderControllerTest {
         mvc.perform(get("/getDataFromResource")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(itemDTOList.size())));
+                .andExpect(jsonPath("$", hasSize(itemDTOList.size())))
+                .andExpect(jsonPath("$[0].id", is(itemDTO.getId())));
     }
 
 }
